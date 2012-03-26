06aaad251c62ff89d082f67fdde8d0c30644f778
Andrew Crump <andrew@kotirisoftware.com>
2012-03-26 17:19:49 +0100

    FC016: LWRP does not declare a default action

--- resources/rr.rb
+++ resources/rr.rb
@@ -28,3 +28,8 @@ attribute :username, :kind_of => String
 attribute :password, :kind_of => String
 attribute :customer, :kind_of => String
 attribute :zone, :kind_of => String
+
+def initialize(*args)
+  super
+  @action = :create
+end
