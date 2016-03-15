2df66827c66675697612c7d1b298767734bc5b3d
Tim Smith <tsmith84@gmail.com>
2016-03-15 12:11:11 -0700

    Add long_description to the metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description "LWRP for managing DNS records with Dynect's REST API"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.10'
 
 recipe 'dynect', 'Installs the dynect_rest RubyGem'
