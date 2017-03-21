03ac67fcc58df77ee3dae4b6321d30f84d7cd39d
Tim Smith <tsmith@chef.io>
2017-03-21 15:53:04 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'dynect'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description "LWRP for managing DNS records with Dynect's REST API"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
