9452b9bd0baafaf450dd11f74fc02dc08f017fc9
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:09:48 -0700

    Fixes from Cookstyle 5.8 and Chef 15 licensing support in test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description "LWRP for managing DNS records with Dynect's REST API"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.0'
-
-recipe 'dynect', 'Installs the dynect_rest RubyGem'
-recipe 'dynect::ec2', 'Dynamically configures Dyn resource records for EC2 nodes based on instance ID and prepopulated attributes on the node'
-recipe 'dynect::a_record', 'Example resource usage to configure an A record'
 
 depends 'build-essential', '>= 5.0'
 
@@ -18,4 +13,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/dynect'
 issues_url 'https://github.com/chef-cookbooks/dynect/issues'
-chef_version '>= 12.7' if respond_to?(:chef_version)
+chef_version '>= 12.7'
