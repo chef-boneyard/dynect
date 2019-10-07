4b7c0532bb84e4f4d31d96bda93f4424de1c8c01
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:18:11 -0700

    Update copyrights to current
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/a_record.rb
+++ recipes/a_record.rb
@@ -2,7 +2,7 @@
 # Cookbook:: dynect
 # Recipe:: a_record
 #
-# Copyright:: 2010-2018, Chef Software, Inc <legal@chef.io>
+# Copyright:: 2010-2019, Chef Software, Inc <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- resources/rr.rb
+++ resources/rr.rb
@@ -3,7 +3,7 @@
 # Resource:: rr
 #
 # Author:: Adam Jacob <adam@chef.io>
-# Copyright:: 2010-2016, Chef Software, Inc <legal@chef.io>
+# Copyright:: 2010-2019, Chef Software, Inc <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
